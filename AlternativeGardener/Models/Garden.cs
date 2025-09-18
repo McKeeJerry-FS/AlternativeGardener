83f3c7589fbf0c7fe7f1a8b82d0361d2a11496a2
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2025-09-18 14:51:09 -0400

    continued building out the Garden model

--- AlternativeGardener/Models/Garden.cs
+++ AlternativeGardener/Models/Garden.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AlternativeGardener.Models.Enums;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 
 namespace AlternativeGardener.Models
@@ -12,17 +13,21 @@ namespace AlternativeGardener.Models
         [Display(Name = "Garden Name")]
         [StringLength(50, ErrorMessage = "Garden name must be longer than 2 characters and cannot be longer than 50 characters.", MinimumLength = 2)]
         public string? GardenName { get; set; }
-        public string? GardenLocation { get; set; }
+        public Location? GardenLocation { get; set; }
         public string? GardenSize { get; set; }
-        public string? GardenType { get; set; }
+        public GardenType? GardenType { get; set; }
 
 
         // Identity User Foreign Key
         public AppUser? User { get; set; }
 
         // Navigation Properties
-        public List<Plant>? Plants { get; set; }
-        public List<Equipment>? EquipmentList { get; set; }
+        public IEnumerable<Plant>? Plants { get; set; }
+        public IEnumerable<Equipment>? EquipmentList { get; set; }
+        public IEnumerable<Nutrient>? Nutrients { get; set; }
+        public IEnumerable<Chemical>? Chemicals { get; set; }
+
+        public IEnumerable<Record>? DailyRecords { get; set; }
 
     }
 }
